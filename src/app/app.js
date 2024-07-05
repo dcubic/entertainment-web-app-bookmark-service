e6696bb61c88fa39845128199123f16e91833d72
unknown <dominikcubic@gmail.com>
2024-07-05 13:08:05 -0700

    :lock: added cors

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
@@ -28,6 +29,7 @@ const healthRouter = new HealthRouter();
 const bookmarksRouter = new BookmarksRouter();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   "/bookmark/health",
